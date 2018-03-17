dc7261c50352c1588aae95abb9687b65a30ee2f1
DanilAe <36730804+DanilAe@users.noreply.github.com>
2018-03-17 22:55:34 +0300

    Update Translate.cs

--- Translate.cs
+++ Translate.cs
@@ -1,5 +1,5 @@
         //Translate some text of C#
-        public static string Translate(string textToTranlate, string lenguage1, string Lenguage2)
+        public static string Translate(string textToTranslate, string lenguage1, string Lenguage2)
         {
             string url = String.Format("https://translate.yandex.net/api/v1.5/tr/translate?key={3}&lang={0}-{1}&text={2}&format=html", this.ApiKey);
             try
